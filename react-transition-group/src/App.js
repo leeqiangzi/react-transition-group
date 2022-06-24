98557ce5765178b0bbc8cb7a349ca5bf7bfd6a45
lizhiqiang17 <lizhiqiang17@58.com>
2022-06-24 18:49:30 +0800

    SwitchTransitionDemo初始化代码添加

--- react-transition-group/src/App.js
+++ react-transition-group/src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import CSSTransitionDemo from "./transition/CSSTransitionDemo";
+import SwitchTransitionDemo from "./transition/SwitchTransitionDemo";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <CSSTransitionDemo/>
+                <SwitchTransitionDemo/>
             </div>
         );
     }
---
+++ react-transition-group/src/transition/SwitchTransitionDemo.js
@@ -0,0 +1,16 @@
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+
+class SwitchTransitionDemo extends PureComponent {
+    render() {
+        return (
+            <div>
+                SwitchTransitionDemo
+            </div>
+        );
+    }
+}
+
+SwitchTransitionDemo.propTypes = {};
+
+export default SwitchTransitionDemo;
