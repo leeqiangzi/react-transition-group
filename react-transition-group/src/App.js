482787665d3f2aced1894900f7bd921297d6a190
lizhiqiang17 <lizhiqiang17@58.com>
2022-06-24 11:19:17 +0800

    初始化app

---
+++ react-transition-group/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>
+                    App2
+                </h2>
+            </div>
+        );
+    }
+}
+
+export default App;
