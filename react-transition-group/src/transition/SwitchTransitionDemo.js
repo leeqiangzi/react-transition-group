44f3de390bc76e0e2c8d993ae48a950738f3a13a
lizhiqiang17 <lizhiqiang17@58.com>
2022-06-27 10:03:25 +0800

    SwitchTransition的使用

--- react-transition-group/src/transition/SwitchTransitionDemo.js
+++ react-transition-group/src/transition/SwitchTransitionDemo.js
@@ -1,11 +1,31 @@
 import React, {PureComponent} from 'react';
-import PropTypes from 'prop-types';
+import {CSSTransition, SwitchTransition} from "react-transition-group";
+import SwitchTransitionCSS from './SwitchTransition.css'
 
 class SwitchTransitionDemo extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOn: true,
+        }
+    }
+
     render() {
+        const {isOn = true} = this.state;
         return (
             <div>
-                SwitchTransitionDemo
+                <SwitchTransition mode={'out-in'}>
+                    {/*in-out  out-in */}
+                    <CSSTransition key={isOn ? 'on' : 'off'}
+                                   classNames={'btn'}
+                                   timeout={1000}
+                    >
+                        <button onClick={e => this.setState({isOn: !isOn})}>
+                            {isOn ? 'on' : 'off'}
+                        </button>
+                    </CSSTransition>
+                </SwitchTransition>
             </div>
         );
     }
